ca28e6fb8e8c7e356e15cef96a3c3523b1a21e54
AlekseyPerov <82451784+AlekseyPerov@users.noreply.github.com>
2021-06-21 04:12:52 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+name = str(input('Введите Ваше имя - '))
+year = int(input('Введите год рождения - '))
+age = int(2021 - year)
+print (name, year, age, 'лет')
---
+++ 3.py
@@ -0,0 +1,4 @@
+a = int(input('Введите число - '))
+b = int(a + a)
+c = int(a + a + a)
+print(int(a + b + c))
